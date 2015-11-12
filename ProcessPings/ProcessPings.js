65375e4e65bc3836f55ec1c8c4f6246cc8f7756e
Elliot Wesoff <elliot@techrockstars.com>
2015-11-13 00:41:37 +0100

    added generic function for writing to db.

--- ProcessPings/ProcessPings.js
+++ ProcessPings/ProcessPings.js
@@ -6,8 +6,10 @@
 var AWS = require('aws-sdk');
 var dynamodb = new AWS.DynamoDB({ apiVersion: "2012-08-10" })
 var s3 = new AWS.S3({apiVersion: '2006-03-01'});
-var jsonData, testCaseId, reportDate, time;
+var jsonData, testCaseId, reportDate, time, hostStats;
 
+// don't retry if failed.  not in dev at least...
+AWS.config.update({ maxRetries: 0 });
 
 exports.handler = function(event, context) {
 
@@ -15,17 +17,18 @@ exports.handler = function(event, context) {
     var srcKey    = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, " "));  
 
     // parse data out of the json file in S3.
-    s3.getObject({Bucket: srcBucket, Key: srcKey}, function(err, data) {
+    s3.getObject({Bucket: srcBucket, Key: srcKey}, function(err, fileData) {
         try {
             if (!err) {
 
-                jsonData    = JSON.parse(data.Body.toString());
+                jsonData    = JSON.parse(fileData.Body.toString());
                 testCaseId  = jsonData['test_case_id'];
                 time        = jsonData['ping_day'];
                 reportDate  = formattedDate(new Date(time));
+                hostStats = jsonData['host_stats'];
 
                 // update host stats.
-                //updateStats();
+                updateStats();
 
                 // add items to the cached ping table.
                 //cachePings();
@@ -44,23 +47,58 @@ exports.handler = function(event, context) {
 };
 
 function updateStats() {
+    for (var i in hostStats) {
+        var host = hostStats[i];
+        var packetLoss = host['packet_loss'];
+        var totalPings = host['total_pings'];
+
+        var data = {
+            "testCaseId" : { "S" : testCaseId.toString() },
+            "reportDate" : { "S" : reportDate }
+        };
+
+        writeItem({ tableName: "HostStat", object: data })
+    }
+}
+
+
+function writeItem(options) {
+    // write an item to the database as specified by the 'tableName' option.
+    
+    console.log('writing to table: ' + options['tableName']);
+    console.log('data: ' + options['object']);
+
+    if (!options['tableName'] || !options['object']) throw 'Insufficient parameters provided for database write.';
+
     var params = {
-        TableName: "HostStat",
-        Key: {
-            "TestCaseId": testCaseId.toString(),
-            "ReportDate": reportDate.toString()
-        }
+        TableName: options['tableName'],
+        Item: options['object']
     };
 
-    dynamodb.getItem(params, function(err, data) {
+    dynamodb.putItem(params, function(err, data) {
         if (!err) {
-            // combine host stats.
-            console.log('Host stat data:');
+            console.log('Successfully wrote data to the ' + tableName + ' table:');
             console.log(data);
         } else {
-            throw "An error occurred getting the host stats from the database: " + err.toString();
+            throw "An error occurred writing to the database: " + err.toString();
         }
     });
+}
+
+function formattedDate(input) {
+    try {
+
+        var date = input ? new Date(input) : new Date();
+        var m_names = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"];
+        var curr_date = date.getDate();
+        var curr_month = m_names[date.getMonth()];
+        var curr_year = date.getFullYear();
+        if (curr_date < 10) curr_date = "0" + curr_date;
+        return curr_year + "-" + curr_month + "-" + curr_date;
+
+    } catch(err) {
+        throw "the submitted date format was incorrect --- " + err.message;
+    }
 }
 
 function cachePings() {
@@ -85,20 +123,4 @@ function cachePings() {
             //console.log(JSON.stringify(data, null, 2));
     //});
 
-}
-
-function formattedDate(input) {
-    try {
-
-        var date = input ? new Date(input) : new Date();
-        var m_names = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"];
-        var curr_date = date.getDate();
-        var curr_month = m_names[date.getMonth()];
-        var curr_year = date.getFullYear();
-        if (curr_date < 10) curr_date = "0" + curr_date;
-        return curr_year + "-" + curr_month + "-" + curr_date;
-
-    } catch(err) {
-        throw "the submitted date format was incorrect --- " + err.message;
-    }
 }
