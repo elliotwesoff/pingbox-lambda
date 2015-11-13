0e8be3bec4df19e811b31a232ec6b23fa620fa48
Elliot Wesoff <elliot@techrockstars.com>
2015-11-13 19:33:26 +0100

    almost got the updateStats method working. turning it off for now, we may be taking another data caching route.

--- ProcessPings/ProcessPings.js
+++ ProcessPings/ProcessPings.js
@@ -5,16 +5,21 @@
 
 var AWS = require('aws-sdk');
 var dynamodb = new AWS.DynamoDB({ apiVersion: "2012-08-10" })
+var dynamodbDoc = new AWS.DynamoDB.DocumentClient();
 var s3 = new AWS.S3({apiVersion: '2006-03-01'});
-var jsonData, testCaseId, reportDate, time, hostStats;
+var async = require('async');
+var srcBucket, srcKey, jsonData, testCaseId, reportDate, time, hostStats;
 
-// don't retry if failed.  not in dev at least...
-AWS.config.update({ maxRetries: 0 });
+// set up configuration...
+AWS.config.update({ 
+    maxRetries: 0,
+    region: 'us-east-1'
+});
 
 exports.handler = function(event, context) {
 
-    var srcBucket = event.Records[0].s3.bucket.name;
-    var srcKey    = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, " "));  
+    srcBucket = event.Records[0].s3.bucket.name;
+    srcKey    = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, " "));  
 
     // parse data out of the json file in S3.
     s3.getObject({Bucket: srcBucket, Key: srcKey}, function(err, fileData) {
@@ -28,7 +33,7 @@ exports.handler = function(event, context) {
                 hostStats = jsonData['host_stats'];
 
                 // update host stats.
-                updateStats();
+                //updateStats();
 
                 // add items to the cached ping table.
                 //cachePings();
@@ -46,22 +51,6 @@ exports.handler = function(event, context) {
     });
 };
 
-function updateStats() {
-    for (var i in hostStats) {
-        var host = hostStats[i];
-        var packetLoss = host['packet_loss'];
-        var totalPings = host['total_pings'];
-
-        var data = {
-            "TestCaseId" : { "S" : testCaseId.toString() },
-            "ReportDate" : { "S" : reportDate }
-        };
-
-        writeItem({ tableName: "HostStat", object: data })
-    }
-}
-
-
 function writeItem(options) {
     // write an item to the database as specified by the 'tableName' option.
     
@@ -75,12 +64,33 @@ function writeItem(options) {
         Item: options['object']
     };
 
-    dynamodb.putItem(params, function(err, data) {
+    dynamodbDoc.put(params, function(err, data) {
         if (!err) {
             console.log('Successfully wrote data to the ' + options['tableName'] + ' table:');
             console.log(data);
         } else {
             throw "An error occurred writing to the database: " + err.toString();
+        }
+    });
+}
+
+function getItem(options) {
+    // retrieve an item from the database.
+    
+    if (!options['tableName'] || !options['object']) throw 'Insufficient parameters provided for database read.';
+
+    var params = {
+        TableName: options['tableName'],
+        Key: options['object']
+    };
+
+    dynamodbDoc.get(params, function(err, data) {
+        if (!err) {
+            console.log('Retrieved item from ' + options['tableName'] + ' table:');
+            console.log(data);
+            return data.Body;
+        } else {
+            throw "An error occurred reading from the database: " + err.toString();
         }
     });
 }
@@ -101,26 +111,67 @@ function formattedDate(input) {
     }
 }
 
-function cachePings() {
-    //var params = {
-        //TableName: "CachedPing",
-        //Key: {
-            //"TestCaseId": testCaseId,
-            //"ReportDate": reportDate
-        //},
+//function updateStats() {
+    //// first, we must look for a report date that already exists for
+    //// this test case. if it doesn't exist, create it.  if it exists,
+    //// update our reply stats with what he have received in this current file.
 
-        //UpdateExpression: "SET " + time.toString() + " = :data",
-        //ExpressionAttributeValues: { 
-            //":data": data
-        //},
-        //ReturnValues: "ALL_NEW"
+    //var data = {
+        //"TestCaseId" : testCaseId.toString(),
+        //"ReportDate" : reportDate
     //};
 
-    //dynamodb.updateItem(params, function(err, data) {
-        //if (err)
-            //console.log(JSON.stringify(err, null, 2));
-        //else
-            //console.log(JSON.stringify(data, null, 2));
-    //});
+    //async.waterfall([
+            //function queryDatabase(next) {
+                ////var dbHostStats = getItem({ tableName: "HostStat", object: data });
+                //dynamodbDoc.get(params, function(err, data) {
+                    //if (!err) {
+                        //console.log('Retrieved item from ' + options['tableName'] + ' table:');
+                        //console.log(data);
+                        //next(data.Body);
+                    //} else {
+                        //throw "An error occurred writing to the database: " + err.toString();
+                    //}
+                //});
+                //next(null, dbHostStats);
+            //}, 
 
-}
+            //function processData(dbHostStats, next) {
+
+                //console.log('REALITY CHECK:');
+                //console.log(dbHostStats);
+
+                //for (var i in hostStats) {
+                    //// organize our data so each host appears as a 'column' in the database.
+                    //var host = hostStats[i];
+                    //delete host.total_failed_pings;
+                    //delete host.total_successful_pings;
+
+                    //data[host.host_name] = host;
+                //}
+
+                //// check to see if the object already exists in the database.
+                //if (Object.keys(dbHostStats).length > 0) {
+
+                    //// update the stats with what we've received in the file.
+                    //for (var i in dbHostStats.Item) {
+                        //var host = dbHostStats.Item[i];
+                        //var hostData = Object.keys(a)[0];
+                        //host['total_pings'] += data[hostData['host_name']]['total_pings'];
+                        //host['packet_loss'] += data[hostData['host_name']]['packet_loss'];
+                    //}
+
+                    //writeItem({ tableName: "HostStat", object: dbHostStats.Item })
+                //} else {
+                    //// create a new record.
+                    //writeItem({ tableName: "HostStat", object: data })
+                //}
+            //}
+    //], function(err) {
+        //if (err) throw err;
+        //else console.log('Stats updating operation completed successfully.');
+    //});
+//}
+
+//function cachePings() {
+//}
